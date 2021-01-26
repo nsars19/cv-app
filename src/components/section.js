57e072dd1caabf23bf3b076e8beab3f342af694a
Nick Sardonini <nsardonini@gmail.com>
2021-01-25 21:43:42 -0500

    add section component
    abstracts behavior from each of the previously created sections

---
+++ src/components/section.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Form from "./form";
+import uniqid from 'uniqid';
+
+class Section extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: true,
+      userInfo: this.setUserInfo(),
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleActiveStatus = this.toggleActiveStatus.bind(this);
+    this.buildElement = this.buildElement.bind(this);
+  }
+
+  setUserInfo() {
+    const keys = Object.keys(this.props.inputs);
+    const userInfo = {};
+
+    keys.forEach((key) => (userInfo[key] = ""));
+    return userInfo;
+  }
+
+  toggleActiveStatus() {
+    this.setState({
+      active: !this.state.active,
+    });
+  }
+
+  handleSubmit() {}
+
+  buildElement(item) {
+    const input = this.props.inputs;
+    const text = input[item[0]].text;
+
+    return <p key={uniqid()}>{item[1] || text}</p>;
+  }
+
+  render() {
+    const inputs = this.props.inputs;
+    const formActive = this.state.active;
+
+    if (formActive) {
+      return (
+        <div className={this.props.sectionClass}>
+          <Form
+            userInfo={this.state.userInfo}
+            formInfo={inputs}
+            toggleActiveStatus={this.toggleActiveStatus.bind(this)}
+            onSubmit={this.handleSubmit.bind(this)}
+          />
+        </div>
+      );
+    } else {
+      const info = Object.entries(this.state.userInfo);
+
+      return (
+        <div className={this.props.sectionClass} onClick={this.toggleActiveStatus}>
+          {info.map((item) => this.buildElement(item))}
+        </div>
+      );
+    }
+  }
+}
+
+export default Section;

7ef05a9269cefb3b813fbed55cd592a45036ed11
Nick Sardonini <nsardonini@gmail.com>
2021-01-25 21:47:15 -0500

    refactor App.js to use Section components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import General from './components/general';
-import Work from './components/work';
-import Education from './components/education';
-import Hobbies from './components/hobbies';
+import Section from './components/section';
+import { general, work, education, hobbies } from './inputs';
 
 function App() {
+  
   return (
     <div className="app">
-      <General />     
-      <Work />
-      <Education />
-      <Hobbies />
+      <Section inputs={general} sectionClass="info-general" />
+      <Section inputs={work} sectionClass="info-work" />
+      <Section inputs={education} sectionClass="info-education" />
+      <Section inputs={hobbies} sectionClass="info-hobbies" />
     </div>
   );
 }

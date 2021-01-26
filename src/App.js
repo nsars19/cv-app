5c6fbe5003040766317213ba48b78649d3edcb7c
Nick Sardonini <nsardonini@gmail.com>
2021-01-26 12:18:43 -0500

    refactor into class structure. add logic to add additional Category components
    Adds a button to each section (except general info) that appends
    a Category component into that section.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,114 @@
+import React from "react";
 import Category from "./components/category";
+import uniqid from "uniqid";
 import { general, work, education, hobbies } from "./inputs";
 
-function App() {
-  return (
-    <div className="app">
-      <section className="section">
-        <Category inputs={general} sectionClass="info-general" />
-      </section>
-      <section className="section">
-        <h2>Work Experience</h2>
-        <Category inputs={work} sectionClass="info-work" />
-      </section>
-      <section className="section">
-        <h2>education</h2>
-        <Category inputs={education} sectionClass="info-education" />
-      </section>
-      <section className="section">
-        <h2>Hobbies</h2>
-        <Category inputs={hobbies} sectionClass="info-hobbies" />
-      </section>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      general: [
+        <Category
+          inputs={general}
+          sectionClass="info-general"
+          key={uniqid()}
+        />,
+      ],
+      work: [
+        <Category 
+          inputs={work} 
+          sectionClass="info-work" 
+          key={uniqid()} 
+        />,
+      ],
+      education: [
+        <Category
+          inputs={education}
+          sectionClass="info-education"
+          key={uniqid()}
+        />,
+      ],
+      hobbies: [
+        <Category
+          inputs={hobbies}
+          sectionClass="info-hobbies"
+          key={uniqid()}
+        />,
+      ],
+    };
+  }
+
+  buildCategory(inputs, className) {
+    return <Category inputs={inputs} className={className} key={uniqid()} />;
+  }
+
+  appendSection(stateKey, inputs, className) {
+    this.setState({
+      [stateKey]: this.state[stateKey].concat(
+        this.buildCategory(inputs, className)
+      ),
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        {/* GENERAL INFO */}
+        <section className="section-general">
+          {this.state.general.map((item) => item)}
+        </section>
+
+        {/* WORK INFO */}
+        <section className="section-work">
+          <h2>Work Experience</h2>
+          <div className="work-wrap" id="work-exp">
+            {this.state.work.map((item) => item)}
+          </div>
+          <button
+            onClick={this.appendSection.bind(this, "work", work, "info-work")}
+          >
+            New +
+          </button>
+        </section>
+
+        {/* EDUCATION INFO */}
+        <section className="section-education">
+          <h2>Education</h2>
+          <div className="edu-wrap" id="edu-exp">
+            {this.state.education.map((item) => item)}
+          </div>
+          <button
+            onClick={this.appendSection.bind(
+              this,
+              "education",
+              education,
+              "info-education"
+            )}
+          >
+            New +
+          </button>
+        </section>
+
+        {/* HOBBY INFO */}
+        <section className="section-hobbies">
+          <h2>Hobbies</h2>
+          <div className="hobbies-wrap">
+            {this.state.hobbies.map((item) => item)}
+          </div>
+          <button
+            onClick={this.appendSection.bind(
+              this,
+              "hobbies",
+              hobbies,
+              "info-hobbies"
+            )}
+          >
+            New +
+          </button>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;

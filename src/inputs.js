4d985c1a1df7ab314ca0f78f4840d6578ab7e014
Nick Sardonini <nsardonini@gmail.com>
2021-01-25 21:44:08 -0500

    add input objects file and export to App.js
    adds input objects for use as props to be fed into Section components

---
+++ src/inputs.js
@@ -0,0 +1,81 @@
+export const general = {
+  name: {
+    stateKey: "name",
+    text: "Name",
+    type: "text",
+  },
+  phoneNum: {
+    stateKey: "phoneNum",
+    text: "Phone Number",
+    type: "text",
+  },
+  email: {
+    stateKey: "email",
+    text: "Email",
+    type: "email",
+  },
+  address: {
+    stateKey: "address",
+    text: "Address",
+    type: "text",
+  },
+};
+
+export const work = {
+  companyName: {
+    stateKey: "companyName",
+    text: "Company",
+    type: "text",
+  },
+  jobTitle: {
+    stateKey: "jobTitle",
+    text: "Job Title",
+    type: "text",
+  },
+  startDate: {
+    stateKey: "startDate",
+    text: "Start Date",
+    type: "date",
+  },
+  endDate: {
+    stateKey: "endDate",
+    text: "End Date",
+    type: "date",
+  },
+  tasks: {
+    stateKey: "tasks",
+    text: "Tasks",
+    type: "text",
+  },
+}
+
+export const education = {
+  schoolName: {
+    stateKey: "schoolName",
+    text: "School",
+    type: "text",
+  },
+  degree: {
+    stateKey: "degree",
+    text: "Degree Achieved",
+    type: "text",
+  },
+  startDate: {
+    stateKey: "startDate",
+    text: "Start Date",
+    type: "date",
+  },
+  endDate: {
+    stateKey: "endDate",
+    text: "End Date",
+    type: "date",
+  },
+}
+
+export const hobbies = {
+  hobby: {
+    stateKey: "hobby",
+    text: "Hobby",
+    type: "text",
+  },
+}
